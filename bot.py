bba0436bfc0b15d2f03b64c69f3cec3941582941
Pedro Gabriel <ggpedro1606@gmail.com>
2020-04-24 01:33:24 -0300

    correcoes

--- bot.py
+++ bot.py
@@ -10,8 +10,9 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument = ("--headless")
 chrome_options.add_argument = ("--disable-dev-shm-usage")
 chrome_options.add_argument = ("--no-sandbox")
-browser = webdriver.Chrome(os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options) 
-print('estou configurado')
+browser = webdriver.Chrome('/app/.chromedriver/bin/chromedriver', chrome_options=chrome_options)
+
+print('configurado !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
 
 site = browser.get('https://www.google.com.br')
 print(site)
